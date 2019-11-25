dca52bef698bd055a083340723fa8f8e50b10782
Dadino <corsalini.david@gmail.com>
2019-11-25 15:11:22 +0100

    - add: unitTests (wip)

--- core/src/test/java/com/dadino/quickstart3/core/QuickLoopTests.kt
+++ core/src/test/java/com/dadino/quickstart3/core/QuickLoopTests.kt
@@ -1,11 +1,12 @@
 package com.dadino.quickstart3.core
 
 import com.dadino.quickstart3.core.components.QuickLoop
+import com.dadino.quickstart3.core.entities.Event
 import io.reactivex.observers.TestObserver
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.MockitoAnnotations
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -14,14 +15,25 @@ class QuickLoopTests {
 		RxJavaSchedulerConfigurator.prepareRxJava()
 	}
 
+	@Test
+	fun sendEvent_eventReceived() {
 
-	@Before
-	fun prepare() {
-		MockitoAnnotations.initMocks(this)
+		//GIVEN
+		val updater = TestStateUpdater(false)
+		val quickLoop = QuickLoop("testloop", updater)
+		val updaterSpy = Mockito.spy(updater)
+
+		//WHEN
+		quickLoop.receiveEvent(TestEntities.Add1ToCounter)
+		quickLoop.receiveEvent(TestEntities.Add1ToCounter)
+
+		//THEN
+		Thread.sleep(1000)
+		verify(updaterSpy, Mockito.times(2)).internalUpdate(Any<TestState>().any(), Any<Event>().any())
 	}
 
 	@Test
-	fun sendEvent_eventReceived() {
+	fun sendEvent_updateState() {
 
 		//GIVEN
 		val quickLoop = QuickLoop("testloop", TestStateUpdater(false))
@@ -31,16 +43,15 @@ class QuickLoopTests {
 		quickLoop.states
 				.toObservable()
 				.subscribe(testObserver)
+		quickLoop.receiveEvent(TestEntities.Add1ToCounter)
+		quickLoop.receiveEvent(TestEntities.Add1ToCounter)
 
 		//THEN
-		//testObserver.assertValue(TestState())
+		Thread.sleep(1000)
+		testObserver.assertValueAt(0) { it.counter == 1 }
+		testObserver.assertValueAt(1) { it.counter == 2 }
 
-		quickLoop.receiveEvent(TestEntities.Add1ToCounter)
-		testObserver.assertValue { it.counter == 1 }
-		testObserver.assertNotComplete()
-
-		quickLoop.receiveEvent(TestEntities.Add1ToCounter)
-		testObserver.assertValue { it.counter == 2 }
 		testObserver.assertNotComplete()
 	}
+
 }
