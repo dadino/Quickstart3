f04f5da3976df5e53b6be7c13e638615b656b017
Dadino <corsalini.david@gmail.com>
2020-04-06 13:04:37 +0200

    - v0.7.1 (26) - add: fragment back handling

--- core/src/main/java/com/dadino/quickstart3/core/fragments/BaseSettingsFragment.kt
+++ core/src/main/java/com/dadino/quickstart3/core/fragments/BaseSettingsFragment.kt
@@ -1,22 +1,26 @@
 package com.dadino.quickstart3.core.fragments
 
+import android.content.Context
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.preference.PreferenceFragmentCompat
-import com.dadino.quickstart3.core.components.AttachedComponent
-import com.dadino.quickstart3.core.components.AttachedComponentController
-import com.dadino.quickstart3.core.components.EventManager
-import com.dadino.quickstart3.core.entities.Signal
-import com.dadino.quickstart3.core.entities.State
-import com.dadino.quickstart3.core.entities.VMStarter
-
+import com.dadino.quickstart3.core.components.*
+import com.dadino.quickstart3.core.entities.*
 
 abstract class BaseSettingsFragment : PreferenceFragmentCompat() {
+
+	protected val backCallback: OnBackPressedCallback = object : OnBackPressedCallback(false) {
+		override fun handleOnBackPressed() {
+			doOnBackPress()
+		}
+	}
+
 	protected val eventManager: EventManager = EventManager()
 	protected val components: AttachedComponentController by lazy {
 		AttachedComponentController(lifecycleOwner = this,
-				eventManager = eventManager,
-				renderFun = { state -> renderState(state) },
-				respondFun = { signal -> respondTo(signal) })
+									eventManager = eventManager,
+									renderFun = { state -> renderState(state) },
+									respondFun = { signal -> respondTo(signal) })
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +29,14 @@ abstract class BaseSettingsFragment : PreferenceFragmentCompat() {
 		components.attachViewModels(viewModels())
 	}
 
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		requireActivity().onBackPressedDispatcher.addCallback(this, backCallback)
+	}
+
 	open fun components(): List<AttachedComponent> = listOf()
 	open fun viewModels(): List<VMStarter> = listOf()
 	open fun renderState(state: State) {}
 	open fun respondTo(signal: Signal) {}
+	open fun doOnBackPress() {}
 }
