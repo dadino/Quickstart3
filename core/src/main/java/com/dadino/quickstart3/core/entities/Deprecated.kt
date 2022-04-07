55470940b12e5eb3df5cc8a43e91f008d1e5a557
David Corsalini <corsalini.david@gmail.com>
2022-04-07 12:49:20 +0200

    - v0.12.5

--- core/src/main/java/com/dadino/quickstart3/core/entities/Deprecated.kt
+++ core/src/main/java/com/dadino/quickstart3/core/entities/Deprecated.kt
@@ -17,12 +17,3 @@ sealed class Operation
 
 @Deprecated(message = "Use Optional from the Quickstart3.base library", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("Optional", imports = ["com.dadino.quickstart3.base.Optional"]))
 sealed class Optional
-
-@Deprecated(message = "Use ContextFormattable from the Quickstart3.contextformattable library", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("ContextFormattable", imports = ["com.dadino.quickstart3.contextformattable.ContextFormattable"]))
-sealed class ContextFormattable
-
-@Deprecated(message = "Use StringFormattable from the Quickstart3.contextformattable library", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("StringFormattable", imports = ["com.dadino.quickstart3.contextformattable.StringFormattable"]))
-sealed class StringFormattable
-
-@Deprecated(message = "Use HtmlFormattable from the Quickstart3.contextformattable library", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("HtmlFormattable", imports = ["com.dadino.quickstart3.contextformattable.HtmlFormattable"]))
-sealed class HtmlFormattable
