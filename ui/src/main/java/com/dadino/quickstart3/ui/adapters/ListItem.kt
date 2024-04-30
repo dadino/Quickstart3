ffac042bd6b4b024b1521e8f3243cd4a09bee37a
David Corsalini <corsalini.david@gmail.com>
2024-04-30 16:06:50 +0200

    - v0.13.4 - add: indent to ListItem

--- ui/src/main/java/com/dadino/quickstart3/ui/adapters/ListItem.kt
+++ ui/src/main/java/com/dadino/quickstart3/ui/adapters/ListItem.kt
@@ -7,67 +7,68 @@ import androidx.annotation.LayoutRes
 
 abstract class ListItem {
 
-	var selected: Boolean = false
-	var selectionType: SelectionType = SelectionType.NoSelection
-	var showInCard: Boolean = false
+  var selected: Boolean = false
+  var selectionType: SelectionType = SelectionType.NoSelection
+  var showInCard: Boolean = false
+  var indent: Int = 0
 
-	@IntegerRes
-	var spanSizeRes: Int? = null
+  @IntegerRes
+  var spanSizeRes: Int? = null
 
-	abstract fun numericId(): Long
+  abstract fun numericId(): Long
 
-	@LayoutRes
-	abstract fun getLayoutId(): Int
+  @LayoutRes
+  abstract fun getLayoutId(): Int
 
-	fun createUpdateBundle(oldItem: ListItem): Bundle? {
-		val diff = Bundle()
+  fun createUpdateBundle(oldItem: ListItem): Bundle? {
+	val diff = Bundle()
 
-		createUpdateBundleForItem(diff, oldItem)
-		createBaseUpdateBundle(diff, oldItem)
+	createUpdateBundleForItem(diff, oldItem)
+	createBaseUpdateBundle(diff, oldItem)
 
-		return if (diff.size() == 0) {
-			null
-		} else diff
+	return if (diff.size() == 0) {
+	  null
+	} else diff
+  }
+
+  fun canGenerateHolder(viewType: Int): Boolean = viewType == getLayoutId()
+
+  fun generateHolder(getView: (Int) -> View): ListItemHolder {
+	return generateHolderForItem(getView(getLayoutId()))
+  }
+
+  abstract fun generateHolderForItem(view: View): ListItemHolder
+
+  protected abstract fun createUpdateBundleForItem(diff: Bundle, oldItem: ListItem)
+  private fun createBaseUpdateBundle(diff: Bundle, oldItem: ListItem) {
+	if (this.selected != oldItem.selected) {
+	  diff.putBoolean(PAYLOAD_SELECTED, this.selected)
 	}
-
-	fun canGenerateHolder(viewType: Int): Boolean = viewType == getLayoutId()
-
-	fun generateHolder(getView: (Int) -> View): ListItemHolder {
-		return generateHolderForItem(getView(getLayoutId()))
+	if (this.selectionType != oldItem.selectionType) {
+	  diff.putInt(PAYLOAD_SELECTABLE, this.selectionType.id)
 	}
-
-	abstract fun generateHolderForItem(view: View): ListItemHolder
-
-	protected abstract fun createUpdateBundleForItem(diff: Bundle, oldItem: ListItem)
-	private fun createBaseUpdateBundle(diff: Bundle, oldItem: ListItem) {
-		if (this.selected != oldItem.selected) {
-			diff.putBoolean(PAYLOAD_SELECTED, this.selected)
-		}
-		if (this.selectionType != oldItem.selectionType) {
-			diff.putInt(PAYLOAD_SELECTABLE, this.selectionType.id)
-		}
-		if (this.showInCard != oldItem.showInCard) {
-			diff.putString(PAYLOAD_CARD, PAYLOAD_CARD)
-		}
-		if (this.spanSizeRes != oldItem.spanSizeRes) {
-			diff.putString(PAYLOAD_SPAN_SIZE, PAYLOAD_SPAN_SIZE)
-		}
+	if (this.showInCard != oldItem.showInCard || this.indent != oldItem.indent) {
+	  diff.putString(PAYLOAD_CARD, PAYLOAD_CARD)
 	}
-
-	fun isContentTheSame(oldItem: ListItem): Boolean {
-		return this.selected == oldItem.selected
-				&& this.selectionType == oldItem.selectionType
-				&& this.showInCard == oldItem.showInCard
-				&& this.spanSizeRes == oldItem.spanSizeRes
-				&& this == oldItem
+	if (this.spanSizeRes != oldItem.spanSizeRes) {
+	  diff.putString(PAYLOAD_SPAN_SIZE, PAYLOAD_SPAN_SIZE)
 	}
+  }
 
-	companion object {
+  fun isContentTheSame(oldItem: ListItem): Boolean {
+	return this.selected == oldItem.selected
+		&& this.selectionType == oldItem.selectionType
+		&& this.showInCard == oldItem.showInCard
+		&& this.spanSizeRes == oldItem.spanSizeRes
+		&& this == oldItem
+  }
 
-		const val PAYLOAD_SELECTED = "PAYLOAD_SELECTED"
-		const val PAYLOAD_SELECTABLE = "PAYLOAD_SELECTABLE"
-		const val PAYLOAD_CARD = "PAYLOAD_CARD"
-		const val PAYLOAD_SPAN_SIZE = "PAYLOAD_SPAN_SIZE"
-	}
+  companion object {
+
+	const val PAYLOAD_SELECTED = "PAYLOAD_SELECTED"
+	const val PAYLOAD_SELECTABLE = "PAYLOAD_SELECTABLE"
+	const val PAYLOAD_CARD = "PAYLOAD_CARD"
+	const val PAYLOAD_SPAN_SIZE = "PAYLOAD_SPAN_SIZE"
+  }
 }
 
