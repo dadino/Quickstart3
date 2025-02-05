74fdbeb0f308a3cd6b27ff717e320536e231fe6e
David Corsalini <corsalini.david@gmail.com>
2025-02-05 12:35:46 +0100

    - v0.14.10 - fix: crash when pressing back quickly while in a flow

--- flow/src/main/java/com/dadino/quickstart3/flow/Flow.kt
+++ flow/src/main/java/com/dadino/quickstart3/flow/Flow.kt
@@ -4,80 +4,86 @@ import com.dadino.quickstart3.base.Event
 import timber.log.Timber
 
 abstract class Flow<FLOW : Flow<FLOW, STATE, STEP>, STATE, STEP : FlowStep<STATE>>(protected val root: FlowStep<STATE>, protected val steps: List<STEP>) {
-	constructor(root: STEP) : this(root = root, steps = listOf(root))
+  constructor(root: STEP) : this(root = root, steps = listOf(root))
 
-	fun hasRemainingSteps() = steps.isNotEmpty()
-	fun getCurrentStep(): STEP? {
-		return steps.lastOrNull()
-	}
+  fun hasRemainingSteps() = steps.isNotEmpty()
+  fun getCurrentStep(): STEP? {
+	return steps.lastOrNull()
+  }
 
-	fun onEvent(state: STATE, onEvent: Event): FlowAdvancement<STATE>? {
-		val currentStep = getCurrentStep() ?: throw RuntimeException("Flow is empty")
-		return currentStep.onEvent(state, onEvent)
-	}
+  fun onEvent(state: STATE, onEvent: Event): FlowAdvancement<STATE>? {
+	val currentStep = getCurrentStep() ?: return FlowAdvancement.ExitFlow()
+	return currentStep.onEvent(state, onEvent)
+  }
 
-	fun applyAdvancement(advancement: FlowAdvancement<STATE>?): FLOW {
-		return if (advancement != null) fromAdvancement(advancement) else getFlow()
-	}
+  fun applyAdvancement(advancement: FlowAdvancement<STATE>?): FLOW {
+	return if (advancement != null) fromAdvancement(advancement) else getFlow()
+  }
 
-	private fun fromAdvancement(advancement: FlowAdvancement<STATE>): FLOW {
-		val steps: List<STEP> = when (advancement) {
-			is FlowAdvancement.ExitFlow               -> {
-				Timber.d("----ExitFlow----")
-				listOf()
-			}
-			is FlowAdvancement.GoToRoot               -> {
-				val index = steps.indexOfFirst { root.key == it.key }
-				val temp = arrayListOf<STEP>()
-				temp.addAll(steps.subList(0, index + 1))
-				Timber.d("<---GoToRoot----\n${temp.joinToString("\n") { it.key }}")
-				temp
-			}
-			is FlowAdvancement.GoToStep<STATE, *>     -> {
-				val temp = arrayListOf<STEP>()
-				temp.addAll(steps)
-				advancement.steps.forEach {
-					temp.add(it as STEP)
-				}
-				Timber.d("----GoForward--->\n${temp.joinToString("\n") { it.key }}")
-				temp
-			}
-			is FlowAdvancement.GoBackToStep<STATE, *> -> {
-				val index = when {
-					advancement.steps.isEmpty() -> null
-					advancement.steps.size == 1 -> {
-						val i = steps.indexOfLast { advancement.steps[0].key == it.key }
-						if (i >= 0) i else null
-					}
-					else                        -> advancement.steps.firstOrNull { targetStep -> steps.indexOfLast { stepInFlow -> targetStep.key == stepInFlow.key } >= 0 }?.let { targetStep -> steps.indexOfLast { targetStep.key == it.key } }
-				}
+  private fun fromAdvancement(advancement: FlowAdvancement<STATE>): FLOW {
+	val steps: List<STEP> = when (advancement) {
+	  is FlowAdvancement.ExitFlow               -> {
+		Timber.d("----ExitFlow----")
+		listOf()
+	  }
 
-				if (index != null) {
-					val temp = arrayListOf<STEP>()
-					temp.addAll(steps.subList(0, index + 1))
-					Timber.d("<---GoBack----\n${temp.joinToString("\n") { it.key }}")
-					temp
-				} else {
-					Timber.d("<---GoBack----\nCan't go back, because none of the GoBackToStep steps are in the current steps\n${steps.joinToString("\n") { it.key }}")
-					steps
-				}
-			}
-			is FlowAdvancement.GoBackOneStep          -> {
-				val temp = arrayListOf<STEP>()
-				temp.addAll(steps.subList(0, steps.lastIndex))
-				Timber.d("<---GoBack----\n${temp.joinToString("\n") { it.key }}")
-				temp
-			}
+	  is FlowAdvancement.GoToRoot               -> {
+		val index = steps.indexOfFirst { root.key == it.key }
+		val temp = arrayListOf<STEP>()
+		temp.addAll(steps.subList(0, index + 1))
+		Timber.d("<---GoToRoot----\n${temp.joinToString("\n") { it.key }}")
+		temp
+	  }
+
+	  is FlowAdvancement.GoToStep<STATE, *>     -> {
+		val temp = arrayListOf<STEP>()
+		temp.addAll(steps)
+		advancement.steps.forEach {
+		  temp.add(it as STEP)
+		}
+		Timber.d("----GoForward--->\n${temp.joinToString("\n") { it.key }}")
+		temp
+	  }
+
+	  is FlowAdvancement.GoBackToStep<STATE, *> -> {
+		val index = when {
+		  advancement.steps.isEmpty() -> null
+		  advancement.steps.size == 1 -> {
+			val i = steps.indexOfLast { advancement.steps[0].key == it.key }
+			if (i >= 0) i else null
+		  }
+
+		  else                        -> advancement.steps.firstOrNull { targetStep -> steps.indexOfLast { stepInFlow -> targetStep.key == stepInFlow.key } >= 0 }
+			?.let { targetStep -> steps.indexOfLast { targetStep.key == it.key } }
 		}
 
-		return updateFlowWithSteps(steps)
+		if (index != null) {
+		  val temp = arrayListOf<STEP>()
+		  temp.addAll(steps.subList(0, index + 1))
+		  Timber.d("<---GoBack----\n${temp.joinToString("\n") { it.key }}")
+		  temp
+		} else {
+		  Timber.d("<---GoBack----\nCan't go back, because none of the GoBackToStep steps are in the current steps\n${steps.joinToString("\n") { it.key }}")
+		  steps
+		}
+	  }
+
+	  is FlowAdvancement.GoBackOneStep          -> {
+		val temp = arrayListOf<STEP>()
+		temp.addAll(steps.subList(0, steps.lastIndex))
+		Timber.d("<---GoBack----\n${temp.joinToString("\n") { it.key }}")
+		temp
+	  }
 	}
 
-	abstract fun getFlow(): FLOW
-	abstract fun updateFlowWithSteps(steps: List<STEP>): FLOW
+	return updateFlowWithSteps(steps)
+  }
 
-	override fun toString(): String {
-		return "Flow(root= ${root.key}, steps= ${steps.joinToString(", ") { it.key }})"
-	}
+  abstract fun getFlow(): FLOW
+  abstract fun updateFlowWithSteps(steps: List<STEP>): FLOW
+
+  override fun toString(): String {
+	return "Flow(root= ${root.key}, steps= ${steps.joinToString(", ") { it.key }})"
+  }
 }
 
