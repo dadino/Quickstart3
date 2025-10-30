5b62c322c9b0d42f676a3dc9761a774801280c54
David Corsalini <corsalini.david@gmail.com>
2025-10-30 14:52:34 +0100

    - add: Selectable.kt color

--- selectable/src/main/java/com/dadino/quickstart3/selectable/Selectable.kt
+++ selectable/src/main/java/com/dadino/quickstart3/selectable/Selectable.kt
@@ -1,52 +1,59 @@
 package com.dadino.quickstart3.selectable
 
 import android.os.Parcelable
+import com.dadino.quickstart3.color.ContextColor
 import com.dadino.quickstart3.contextformattable.ContextFormattable
 import com.dadino.quickstart3.icon.ContextDrawable
 import kotlinx.parcelize.Parcelize
 
 interface Selectable : Parcelable {
 
-    fun getSelectionId(): Id
-    fun getMainText(): ContextFormattable
-    fun getSecondaryText(): ContextFormattable? = null
-    fun getSelectionIcon(): ContextDrawable? = null
+  fun getSelectionId(): Id
+  fun getMainText(): ContextFormattable
+  fun getSecondaryText(): ContextFormattable? = null
+  fun getSelectionIcon(): ContextDrawable? = null
+  fun getSelectionColor(): ContextColor? = null
 
-    fun getSelectionEnabled(): Boolean = true
+  fun getSelectionEnabled(): Boolean = true
 }
 
 @Parcelize
 data class SimpleSelectable(
-    private val id: Id,
-    private val mainText: ContextFormattable,
-    private val secondaryText: ContextFormattable? = null,
-    private val icon: ContextDrawable? = null,
-    private val enabled: Boolean = true
+  private val id: Id,
+  private val mainText: ContextFormattable,
+  private val secondaryText: ContextFormattable? = null,
+  private val icon: ContextDrawable? = null,
+  private val color: ContextColor? = null,
+  private val enabled: Boolean = true
 ) : Selectable {
 
-    override fun getSelectionId(): Id {
-        return id
-    }
+  override fun getSelectionId(): Id {
+    return id
+  }
 
-    override fun getMainText(): ContextFormattable {
-        return mainText
-    }
+  override fun getMainText(): ContextFormattable {
+    return mainText
+  }
 
-    override fun getSecondaryText(): ContextFormattable? {
-        return secondaryText
-    }
+  override fun getSecondaryText(): ContextFormattable? {
+    return secondaryText
+  }
 
-    override fun getSelectionIcon(): ContextDrawable? {
-        return icon
-    }
+  override fun getSelectionIcon(): ContextDrawable? {
+    return icon
+  }
 
-    override fun getSelectionEnabled(): Boolean {
-        return enabled
-    }
+  override fun getSelectionEnabled(): Boolean {
+    return enabled
+  }
+
+  override fun getSelectionColor(): ContextColor? {
+    return color
+  }
 }
 
 fun <T : Selectable> List<T>?.find(id: Id?): T? {
-    if (this == null) return null
-    if (id == null) return null
-    return this.firstOrNull { it.getSelectionId() == id }
+  if (this == null) return null
+  if (id == null) return null
+  return this.firstOrNull { it.getSelectionId() == id }
 }
